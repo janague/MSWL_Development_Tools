398836237cc0f5588298dfc404a323dc4921989a
janague <janague@gmail.com>
2013-10-22 23:56:08 +0200

    Example of unit testing

---
+++ examples/unittest/kelvinToFahrenheit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+  
+import kelvinToFahrenheitFunctions as k2f
+
+if __name__ == '__main__':
+    print k2f.KelvinToFahrenheit(273)
+    print k2f.KelvinToFahrenheit(104)
+    print k2f.KelvinToFahrenheit(454)
+    print int(k2f.KelvinToFahrenheit(505.78))
+    print k2f.KelvinToFahrenheit(-5)
---
+++ examples/unittest/kelvinToFahrenheitFunctions.py
@@ -0,0 +1,4 @@
+def KelvinToFahrenheit(Temperature):
+    assert (Temperature >= 0),"Colder than absolute zero!"
+    return ((Temperature-273)*1.8)+32
+
